cd414e798953c2cfa3cd4fcdacb4d7f130d05f19
Ne_Uchi <drmor107@gmail.com>
2026-04-15 01:09:06 +0500

    created navbar component, for now it do nothing

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,24 @@
 function Nav() {
-  return <h1>hello</h1>;
+  return (
+    <nav>
+      <div className="branding">
+        <h1>Odin Shop</h1>
+        <p>made by</p>
+        <a href="https://github.com/drmor">ne_uchi</a>
+      </div>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Shop</a>
+        </li>
+        <li>
+          <a href="#">Cart</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Nav;
